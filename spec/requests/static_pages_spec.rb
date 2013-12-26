5afb6a461517a2de26bb42bdebd94b703b636e4c
AngieChilds <pirateprincess_avh@yahoo.com>
2013-12-26 14:21:38 +0200

    master

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+     end
+end
